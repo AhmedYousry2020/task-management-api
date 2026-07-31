c1ee0549aa18491c2916d75afc3549cb8e8afdd2
Ahmed Yousry <63677504+AhmedYousry2020@users.noreply.github.com>
2026-08-01 01:28:50 +0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use App\Repositories\Contracts\AuthRepositoryInterface;
 use App\Repositories\Eloquent\AuthRepository;
+use App\Repositories\Contracts\AuthRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
--- database/migrations/2026_07_31_203028_create_projects_table.php
+++ database/migrations/2026_07_31_203028_create_projects_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\ProjectStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
